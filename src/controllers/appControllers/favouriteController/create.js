4a16338adbb77e6aaf2d0b34183cfe1f8e79e018
PavloDevee <pavloseniordev@gmail.com>
2025-07-07 18:38:21 +0300

    fix: fix for notifications

--- src/controllers/appControllers/favouriteController/create.js
+++ src/controllers/appControllers/favouriteController/create.js
@@ -1,12 +1,6 @@
-const { sendResponse } = require('@/helpers');
-const schema = require('./schemaValidate');
+const { sendResponse } = require("@/helpers");
+const schema = require("./schemaValidate");
 
-/**
- * Handles the creation and removal of favourites.
- * @param {Object} Model - Mongoose model to interact with the database.
- * @param {Object} req - Express request object.
- * @param {Object} res - Express response object.
- */
 
 const create = async (Model, req, res) => {
   // Attach the admin ID to the request body
@@ -15,7 +9,13 @@ const create = async (Model, req, res) => {
   // Validate the request body against the schema
   const { error, value } = schema.validate(req.body);
   if (error) {
-    return sendResponse(res, 400, false, null, error.details[0]?.message || 'Validation error');
+    return sendResponse(
+      res,
+      400,
+      false,
+      null,
+      error.details[0]?.message || "Validation error"
+    );
   }
 
   const { user, favouriteUser } = value;
@@ -34,13 +34,17 @@ const create = async (Model, req, res) => {
       { removed: true },
       { new: true, runValidators: true }
     );
-    return sendResponse(res, 200, true, null, 'Unfavourited');
+    return sendResponse(res, 200, true, null, "Unfavourited");
   }
 
   // Create a new favourite
   await new Model(value).save();
 
-  return sendResponse(res, 200, true, null, 'Favourited');
+  // Add Notification for favourite
+  const { notification } = require("@/helpers");
+  await notification("favourite", user, favouriteUser);
+
+  return sendResponse(res, 200, true, null, "Favourited");
 };
 
 module.exports = create;
